dd98e348e3a81ad098c421e5c47438cd52343559
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-10-16 14:25:17 +0800

    feat: create book component

---
+++ src/components/Book.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+    image: String,
+    title: String,
+    url: String,
+    price: String,
+    format: String,
+    author: String,
+    bookstore: String
+})
+</script>
+
+<template>
+    <div class="book">
+        <img class="cover" :src="image">
+        <a class="link" :href="`https://${bookstore}.com${url}`">
+            <p class="title">{{ title }}</p>
+        </a>
+        <p class="price">{{ price }}</p>
+        <p class="format">{{ format }}</p>
+        <p class="author">{{ author }}</p>
+        <p class="bookstore">{{ bookstore }}</p>
+    </div>
+</template>
+
+<style scoped>
+.book {
+    flex: 0 1 15%;
+    display: inline-block;
+}
+
+.cover {
+    width: 100%;
+}
+</style>
