b9e14656181e45848538afdefad50218ce112aae
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-10-17 15:13:47 +0800

    feat: isolate best deal & remove duplicates

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,32 +1,69 @@
 <script setup>
-import { RouterLink, useRouter } from 'vue-router';
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
+    import { RouterLink, useRouter } from 'vue-router';
+    import { ref, onMounted } from 'vue';
+    import axios from 'axios';
 
-import Book from '../components/Book.vue';
+    import Book from '../components/Book.vue';
 
-const loading = ref(true)
-const results = ref([])
-const route = useRouter()
-const query = ref(route.currentRoute.value.params.query)
+    const loading = ref(true)
+    const results = ref([])
+    const bestDeal = ref(null)
+    const route = useRouter()
+    const query = ref(route.currentRoute.value.params.query)
 
-async function fetch() {
-    loading.value = true
+    function getBestDeal() {
+        // retrieve first item in sorted booklist
+        bestDeal.value = results.value[0]
 
-    console.log(query.value)
-    results.value = []
-    const response = await axios.get(`https://book-browser-backend.vercel.app/api?q=${query.value}`)
-    for (const bookstore in response.data) {
-        for (const index in response.data[bookstore]) {
-            results.value.push(response.data[bookstore][index])
-        }
+        // remove item from booklist
+        results.value.shift()
     }
-    console.log(results.value)
 
-    loading.value = false
-}
+    function removeDuplicates() {
+        const uniqueArray = []
+        const seenItems = new Set()
 
-onMounted(fetch)
+        for (const item of results.value) {
+            const itemKey = `${item.bookstore}-${item.price}-${item.title}`
+
+            if (!seenItems.has(itemKey)) {
+                seenItems.add(itemKey)
+                uniqueArray.push(item)
+            }
+        }
+
+        results.value = uniqueArray
+    }
+
+    async function fetch() {
+        loading.value = true
+
+        // get list of books
+        results.value = []
+        bestDeal.value = null
+        const response = await axios.get(`https://book-browser-backend.vercel.app/api?q=${query.value}`)
+        for (const bookstore in response.data) {
+            for (const index in response.data[bookstore]) {
+                results.value.push(response.data[bookstore][index])
+            }
+        }
+
+        // sort list of books by price
+        results.value.sort((currentItem, nextItem) => {
+            const currentPrice = parseFloat(currentItem.price.replace('$', ''));
+            const nextPrice = parseFloat(nextItem.price.replace('$', ''));
+            return currentPrice - nextPrice;
+        });
+
+        // remove repeat entries
+        removeDuplicates();
+
+        getBestDeal()
+
+        loading.value = false
+    }
+
+    onMounted(fetch)
 </script>
 
 <template>
@@ -44,63 +81,68 @@ onMounted(fetch)
     </header>
     <main>
         <h2 class="loading-text" v-if="loading">Loading...</h2>
+        <div v-if="bestDeal" class="bestdeal">
+            <h1 class="bestdeal-title">Best deal found:</h1>
+            <Book :image="bestDeal.image" :title="bestDeal.title" :url="bestDeal.url" :price="bestDeal.price"
+                :format="bestDeal.format" :author="bestDeal.author" :bookstore="bestDeal.bookstore" />
+        </div>
         <Book v-for="book in results" :image="book.image" :title="book.title" :url="book.url" :price="book.price"
             :format="book.format" :author="book.author" :bookstore="book.bookstore" />
     </main>
 </template>
 
 <style scoped>
-header {
-    display: flex;
-    align-items: center;
-    padding: 0px 50px;
-}
+    header {
+        display: flex;
+        align-items: center;
+        padding: 0px 50px;
+    }
 
-.logo-link {
-    text-decoration: none;
-}
+    .logo-link {
+        text-decoration: none;
+    }
 
-.search-form {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
+    .search-form {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 
-.searchbar {
-    width: 560px;
-    padding: 10px;
-    border-radius: 5px;
-    border: none;
-    font-size: 16px;
-    font-family: Arvo;
-    border-radius: 5px 0 0 5px;
-}
+    .searchbar {
+        width: 560px;
+        padding: 10px;
+        border-radius: 5px;
+        border: none;
+        font-size: 16px;
+        font-family: Arvo;
+        border-radius: 5px 0 0 5px;
+    }
 
-.search-btn {
-    height: 40px;
-    width: 40px;
-    flex-shrink: 0;
-    border: none;
-    border-radius: 0 5px 5px 0;
-    background-color: var(--contrast);
-}
+    .search-btn {
+        height: 40px;
+        width: 40px;
+        flex-shrink: 0;
+        border: none;
+        border-radius: 0 5px 5px 0;
+        background-color: var(--contrast);
+    }
 
-.search-btn:active {
-    background-color: #83889d;
-}
+    .search-btn:active {
+        background-color: #83889d;
+    }
 
-.search-btn-icon {
-    height: 20px;
-}
+    .search-btn-icon {
+        height: 20px;
+    }
 
-main {
-    margin: 50px;
-    padding: 50px;
-    background-color: var(--text);
-    display: flex;
-    flex-wrap: wrap;
-    gap: max(20px, 0.5%);
-    justify-content: space-between;
-}
+    main {
+        margin: 50px;
+        padding: 50px;
+        background-color: var(--text);
+        display: flex;
+        flex-wrap: wrap;
+        gap: max(20px, 0.5%);
+        justify-content: space-between;
+    }
 </style>
